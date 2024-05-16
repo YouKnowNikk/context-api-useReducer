67b059f1723579db1bd3f85107da4a8d3e0e7a8d
Nikesh Gupta <nikeshgupta130@gmail.com>
2024-05-16 18:07:46 +0530

    Passing focus on Edit

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState ,useRef} from 'react'
 import useTodo from './useTodo';
 function Addtask() {
     const [task, setTask] = useState('');
     const { addTodo, editTodo, state } = useTodo();
     const [error, setError] = useState(false)
+    const inputRef =useRef(null)
     const HandleSubmit = () => {
         if (state.toUpdate) {
             editTodo({ id: state.toUpdate.id, task: task })
@@ -22,13 +23,14 @@ function Addtask() {
     }
     useEffect(() => {
         if (state.toUpdate) {
+            inputRef.current.focus();
             setTask(state.toUpdate.task)
         }
     }, [state.toUpdate])
 
     return (
         <div className='addtask'>
-            <input type='text' onChange={(e) => { setTask(e.target.value); setError(false); }} value={task} className='input' placeholder='enter your daily tasks' />
+            <input  ref ={inputRef}type='text' onChange={(e) => { setTask(e.target.value); setError(false); }} value={task} className='input' placeholder='enter your daily tasks' />
             <button className='addButton' onClick={HandleSubmit}>{state.toUpdate ? 'Save Todo' : 'ADD TODO'}</button>
             {error && <p style={{ color: "red", width: "90%", margin: "0 auto", textAlign: 'center' }}>Please write your task</p>}
         </div>
